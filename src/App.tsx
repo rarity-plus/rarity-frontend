b14da923dfbfe1729835e93ecc20e1379705e188
chiuzon <89428507+chiuzon@users.noreply.github.com>
2021-09-14 15:37:22 +0300

    React Template

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  // Create the count state.
+ 
+  
+  return (
+    <div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Providers.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Providers: React.FC = ({children}) => {
+    
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default Providers;
