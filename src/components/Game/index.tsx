632ef9450f870121acc29bd11ad8632b478883b7
chiuzon <89428507+chiuzon@users.noreply.github.com>
2021-09-19 15:32:04 +0300

    Phaser and MainScene removed

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,37 +1,21 @@
 import React, { createElement, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import Phaser from 'phaser'
-import { IonPhaser, GameInstance } from '@ion-phaser/react'
-import MainScene from './src/MainScene';
-
-import { makeAutoObservable } from 'mobx';
+import { Scene, Engine, FreeCamera, Vector3, HemisphericLight, Mesh } from 'babylonjs';
 import { observer } from "mobx-react-lite"
 
-import { modal } from '../../contexts/Modal';
 
 const GameWrapperPanel = styled.div`
   width: auto;
   height: auto;
-  
   max-height: 100vh;
-  
   overflow: hidden;
 `
 
 const Game = observer(({children}) => {
-
-
-  const adventureBoardModal = createElement(() => {
-    return (
-      <h1>Start an adventure</h1>
-    )
-  })
-
   const panelRef = useRef(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
-  const gameRef = useRef<HTMLIonPhaserElement>(null)
-
-  const [game, setGame] = useState<GameInstance>()
+  const [mainScene, setMainScene] = useState<Scene>()
 
   const [initialize, setInitialize] = useState(false)
 
@@ -40,38 +24,54 @@ const Game = observer(({children}) => {
       return;
     }
 
-    setGame({
-      height: panelRef.current.clientHeight,
-      width: panelRef.current.clientWidth,
-      type: Phaser.AUTO,
-      render: {
-        antialias: false,
-        antialiasGL: false,
-        pixelArt: true,
-        roundPixels: false
-      },
-      physics: {
-        default: 'arcade',
-        arcade: {
-          debug: true
-        }
-      },
-      scene: MainScene
-    })
+    var engine = new Engine(canvasRef.current, true, {preserveDrawingBuffer: true, stencil: true});
+
+    var createScene = function(){
+      // Create a basic BJS Scene object
+      var scene = new Scene(engine);
+      // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
+      var camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
+      // Target the camera to scene origin
+      camera.setTarget(Vector3.Zero());
+      // Attach the camera to the canvas
+      camera.attachControl(canvasRef.current, false);
+      // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
+      var light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
+      // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
+      var sphere = Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
+      // Move the sphere upward 1/2 of its height
+      sphere.position.y = 1;
+      // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
+      var ground = Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+      // Return the created scene
+      return scene;
+    }
+    var scene = createScene();
+
+    setMainScene(scene)
 
     setInitialize(true)
 
+    engine.runRenderLoop(function(){
+      scene.render();
+    });
+// the canvas/window resize event handler
+    window.addEventListener('resize', function(){
+      engine.resize();
+    });
+
+
     return () => {
-      if(gameRef.current){
-        gameRef.current.destroy()
-      }
+
     }
   }, [])
 
   return (
     <GameWrapperPanel ref={panelRef}>
       {children}
-        <canvas id="myCanvas" style={{border: "5px solid #fff", width: "100%", height: "100vh"}}>
+
+      <canvas ref={canvasRef} style={{border: "5px solid #fff", width: "100%", height: "100vh"}}>
+
       </canvas>
 
     </GameWrapperPanel>
