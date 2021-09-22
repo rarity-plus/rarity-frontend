546117055df08d20bce1a482081b1bdbcfc82f2e
chiuzon <89428507+chiuzon@users.noreply.github.com>
2021-09-22 22:37:35 +0300

    DAS

--- src/components/Game/src/scenes/MainScene.ts
+++ src/components/Game/src/scenes/MainScene.ts
@@ -1,6 +1,56 @@
 import RPScene from '../components/RPScene';
 import RPPlayer from '../gameObjects/RPPlayer';
+<<<<<<< HEAD
 import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, WebGLRenderer } from 'three';
+=======
+
+class MainScene extends RPScene {
+
+  light: HemisphericLight;
+  public camera: ArcRotateCamera;
+
+  player: RPPlayer
+  navigationPlugin: RecastJSPlugin;
+
+  onSceneLoaded(scene: Scene) {
+    super.onSceneLoaded(scene);
+
+    try{
+      this.navigationPlugin = new RecastJSPlugin();
+      this.navigationPlugin.setWorkerURL("workers/navMeshWorker.js")
+    }catch (e){
+      console.log(e)
+    }
+
+    this.camera = new ArcRotateCamera("Camera", 1, 1, 4, Vector3.Zero(), this._scene)
+
+    this.camera.attachControl(this._engine.inputElement, false);
+
+    this.light = new HemisphericLight('light1', new Vector3(0, 1, 0), this._scene);
+
+    this.player = new RPPlayer(scene, this.camera)
+
+    let plane = this._scene.getMeshByName("Plane") as Mesh
+
+    // this.navigationPlugin.createNavMesh([plane], {
+    //   ch: 0,
+    //   cs: 0,
+    //   detailSampleDist: 0,
+    //   detailSampleMaxError: 0,
+    //   maxEdgeLen: 0,
+    //   maxSimplificationError: 0,
+    //   maxVertsPerPoly: 0,
+    //   mergeRegionArea: 0,
+    //   minRegionArea: 0,
+    //   walkableClimb: 0,
+    //   walkableHeight: 0,
+    //   walkableRadius: 0,
+    //   walkableSlopeAngle: 0
+    //
+    // })
+
+  }
+>>>>>>> parent of c851827 (Use another path finding library)
 
 class MainScene extends RPScene {
 
