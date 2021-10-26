2823f90157a20db54bd0fe9d06b13f32c13457e2
as <chiuzon@protonmail.com>
2021-10-26 14:53:35 +0300

    web3provider setup

--- src/helpers/web3Helper.ts
+++ src/helpers/web3Helper.ts
@@ -2,7 +2,7 @@ import { ethers } from 'ethers'
 
 export const POLLING_INTERVAL = 12000
 
-export const getLibrary = (provider: any) => {
+export const getWeb3Library = (provider: any) => {
     const library = new ethers.providers.Web3Provider(provider)
     library.pollingInterval = POLLING_INTERVAL
     return library
