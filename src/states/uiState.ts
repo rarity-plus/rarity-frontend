187d29c85610a81b05ef275a482ef0b349b3f469
as <chiuzon@protonmail.com>
2021-10-26 20:57:06 +0300

    modal state

--- src/states/uiState.ts
+++ src/states/uiState.ts
@@ -1,12 +1,32 @@
 import { makeAutoObservable } from "mobx"
+import { ReactNode } from "react"
+import { ModalType } from "./types"
 
 
 export class UIState {
 
-    
+    currentModal: ModalType | null = null
 
     constructor(){
         makeAutoObservable(this)
+    }
+
+    showModal = (title: string, component: ReactNode, closable = true) => {
+        if(!this.currentModal?.closable){
+            return;
+        }
+
+        this.currentModal = {
+            title: title,
+            component: component,
+            closable: closable
+        }
+
+        return this.currentModal
+    }
+
+    closeModal = () => {
+        this.currentModal = null
     }
 }
 
