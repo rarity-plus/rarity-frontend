156eca2b2e08b4032cb39fc440c021926aa6641e
as <chiuzon@protonmail.com>
2021-10-26 14:31:13 +0300

    path aliases

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,5 @@
+const CracoAlias = require("craco-alias");
+
 module.exports = {
     style: {
       postcss: {
@@ -7,4 +9,17 @@ module.exports = {
         ],
       },
     },
+    plugins: [
+      {
+        plugin: CracoAlias,
+        options: {
+          source: "tsconfig",
+          // baseUrl SHOULD be specified
+          // plugin does not take it from tsconfig
+          baseUrl: "./src",
+          // tsConfigPath should point to the file where "baseUrl" and "paths" are specified
+          tsConfigPath: "./tsconfig.json"
+        }
+      }
+    ]
   }
