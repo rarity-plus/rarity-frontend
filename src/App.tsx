924443adbdc41721bcca2bfa043de1dae9d5a938
as <chiuzon@protonmail.com>
2021-10-26 14:05:03 +0300

    tsconfig change for mobx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <div className='bg-black'>ass</div>
       </header>
     </div>
   );
