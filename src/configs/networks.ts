c243c9aa16b5186f5ba25d5929b3d14bb7a6fdca
as <chiuzon@protonmail.com>
2021-10-28 11:50:48 +0300

    useRefresh hook

--- src/configs/networks.ts
+++ src/configs/networks.ts
@@ -1,4 +1,4 @@
-import { NetworksType } from "./types";
+import { NetworkType } from "./types";
 
 export const RPC_NODES = [
     process.env.RPC_NODE_1,
@@ -6,24 +6,22 @@ export const RPC_NODES = [
     process.env.RPC_NODE_3
 ]
 
-const Networks: NetworksType = {
-    "FTM": {
-        name: "Fantom Opera",
-        chainId: 2,
-        rpcNodes: [
-            "https://rpcapi.fantom.network"
-        ],
-        blockExplorer: "https://ftmscan.com/",
-        
-        currencyName: "FTM",
-        currencySymbol: "ftm",
-        currencyDecimals: 18,
+const Networks: NetworkType = {
+    name: "Fantom Opera",
+    chainId: 2,
+    rpcNodes: [
+        "https://rpcapi.fantom.network"
+    ],
+    blockExplorer: "https://ftmscan.com/",
+    
+    currencyName: "FTM",
+    currencySymbol: "ftm",
+    currencyDecimals: 18,
 
-        addresses: {
-            RARITY_CONTRACT: "0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb",
-            RARITY_GOLD_CONTRACT: "0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2",
-            RARITY_ATTRIBUTE_CONTRACT: "0xB5F5AF1087A8DA62A23b08C00C6ec9af21F397a1"
-        }
+    addresses: {
+        RARITY_CONTRACT: "0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb",
+        RARITY_GOLD_CONTRACT: "0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2",
+        RARITY_ATTRIBUTE_CONTRACT: "0xB5F5AF1087A8DA62A23b08C00C6ec9af21F397a1"
     }
 }
 
