d7ded1d1ab66764e02284e2bba21960eba059293
as <chiuzon@protonmail.com>
2021-10-28 13:15:36 +0300

    getRarityTokenURL

--- src/helpers/ftmScanHelper.ts
+++ src/helpers/ftmScanHelper.ts
@@ -2,3 +2,6 @@
 
 export const FTMSCAN_API_KEY = "2VB2G9EGY5156FY3UBVH9JGPINPFBG6I8U"
 
+export const getRarityTokenURL = (account: string) => {
+    return `https://api.ftmscan.com/api?module=account&action=tokennfttx&contractaddress=0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb&address=${account}&apikey=${FTM_APY_KEY}`
+  }
