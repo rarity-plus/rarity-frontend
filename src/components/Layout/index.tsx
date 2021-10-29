471527362d4e25ef0fff91ba3cd740d42c1b5f16
as <chiuzon@protonmail.com>
2021-10-29 15:55:04 +0300

    listen on account change and chain id change and reload

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,28 @@
-import React, { FC } from "react"
+import { useWeb3React } from "@web3-react/core"
+import React, { FC, useEffect, useRef } from "react"
+import { useHistory } from "react-router"
+
 
 const Layout: FC = ({children}) => {
+    const history = useHistory()
+
+    useEffect(() => {
+        const handleChainAccountChanged = () => {
+            history.go(0)
+        }
+
+        (window as any)['ethereum'].on('chainChanged', handleChainAccountChanged);
+        (window as any)['ethereum'].on('accountsChanged', handleChainAccountChanged);
+
+        return () => {
+            (window as any)['ethereum'].removeListener('chainChanged', handleChainAccountChanged)
+            (window as any)['ethereum'].removeListener('accountsChanged', handleChainAccountChanged)
+        }
+    }, [])
+
     return (
         <>
-        {/*ModalListener*/}
+            {/*ModalListener*/}
             {children}
         </>
     )
